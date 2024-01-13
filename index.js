c09da6be43a1bdf427deb543325064d245e5ca5f
Adrija005 <154043328+Adrija005@users.noreply.github.com>
2024-01-14 00:45:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // index.js
 const express = require('express');
 const app = express();
-const port = 80; // Choose your desired port
+const port = 3000; // Choose your desired port
 const connectDB = require('./db');
 
 connectDB(); // Call the function to connect to the database
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:80");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
